1bd6aed503b6a41d620b9bcfab18038013c36edc
newsflash <newsflash@gmail.com>
2012-11-03 16:57:27 +0000

    Add internationalization

--- src/no/hig/sss/sirc/sIRC.java
+++ src/no/hig/sss/sirc/sIRC.java
@@ -1,15 +1,21 @@
 package no.hig.sss.sirc;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 
 public class sIRC {
-
+	private static ResourceBundle messages;
+	private static Locale currentLocale;
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		currentLocale = Locale.getDefault();
+		messages = ResourceBundle.getBundle("i18n/I18N", currentLocale);
+		
+		System.out.println(messages.getString("connectionOptions.dialogTitle"));
 	}
 
 }

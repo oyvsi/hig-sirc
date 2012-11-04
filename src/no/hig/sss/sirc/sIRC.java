10de164b989e24a3612d626734ae141c1e0e3cb7
nils.slaaen <nils.slaaen@gmail.com>
2012-11-04 16:28:58 +0000

    Added JDesktopPane, need refactoring

--- src/no/hig/sss/sirc/sIRC.java
+++ src/no/hig/sss/sirc/sIRC.java
@@ -1,22 +1,52 @@
 package no.hig.sss.sirc;
 
 import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
 
 
 public class sIRC extends JFrame {
 	private static ResourceBundle messages;
 	private static Locale currentLocale;
+	JDesktopPane jdp;
 	
 	public sIRC() {
+		
+		jdp = new JDesktopPane();
 		GUI gui = new GUI(messages);
-		setExtendedState(MAXIMIZED_BOTH);
-		setJMenuBar(gui.menu());		
+		
+        int inset = 50;
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setBounds(inset, inset,
+                  screenSize.width  - inset*2,
+                  screenSize.height - inset*2);
+		createOptions();
+		setContentPane(jdp);
+		setJMenuBar(gui.menu());
+		
+		jdp.setDragMode(JDesktopPane.OUTLINE_DRAG_MODE);
+		
 	}
 	
+	public void createOptions() {
+		JInternalFrame frame = new JInternalFrame("Channel",true,true,true,true);
+
+		
+		frame.setSize(300,300);
+		 
+        //Set the window's location.
+        frame.setLocation(0, 0);
+        frame.setVisible(true); //necessary as of 1.3
+        jdp.add(frame);
+        try {
+            frame.setSelected(true);
+        } catch (java.beans.PropertyVetoException e) {}
+	}
 	/**
 	 * @param args
 	 */
@@ -25,7 +55,7 @@ public class sIRC extends JFrame {
 		messages = ResourceBundle.getBundle("i18n/I18N", currentLocale);
 		
 		sIRC irc = new sIRC();
-		irc.pack();
+		//irc.pack();
 		irc.setVisible(true);
 		irc.setDefaultCloseOperation(EXIT_ON_CLOSE);
 

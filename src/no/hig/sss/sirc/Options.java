522d4eb82e7dd7a2034276baba31271ded587ec2
nils.slaaen <nils.slaaen@gmail.com>
2012-11-14 17:20:33 +0000

    Added options to panels instead:D

--- src/no/hig/sss/sirc/Options.java
+++ src/no/hig/sss/sirc/Options.java
@@ -1,34 +1,3 @@
-/*
- * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- *   - Neither the name of Oracle or the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
- 
 package no.hig.sss.sirc;
  
 import java.awt.*;
@@ -43,12 +12,12 @@ public class Options extends JPanel
     private JLabel picture;
     private JList list;
     private JSplitPane splitPane;
-    private String[] imageNames = { "Connection", "Text", "Font"};
+    private String[] optionNames = { "Connection", "Text", "Font"};
     public Options() {
  
         //Create the list of images and put it in a scroll pane.
          
-        list = new JList(imageNames);
+        list = new JList(optionNames);
         list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         list.setSelectedIndex(0);
         list.addListSelectionListener(this);
@@ -61,7 +30,7 @@ public class Options extends JPanel
         
         JScrollPane pictureScrollPane = new JScrollPane(picture);
         ConnectionOptions.setMessages (ResourceBundle.getBundle ("i18n/I18N"));
-        JInternalFrame co = new ConnectionOptions(); 
+        JPanel co = new ConnectionOptions(); 
         
         //Create a split pane with the two scroll panes in it.
         splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
@@ -103,14 +72,14 @@ public class Options extends JPanel
         //updateLabel(imageNames[list.getSelectedIndex()]);
     }
     void setViewConnectionOptions() {
-    	JInternalFrame co = new ConnectionOptions(); 
+    	JPanel co = new ConnectionOptions(); 
     	splitPane.setRightComponent(co);
     }
     void setViewColorOptions() {
-    	JInternalFrame to = new TextOptions();  
-		TextOptions test = new TextOptions();
+    	JPanel to = new TextOptions();  
 		to.setPreferredSize(new Dimension(800, 600));
-		to.pack();
+		to.setBorder(null);
+		//to.pack();
 		to.setVisible(true);
     	
     	splitPane.setRightComponent(to);

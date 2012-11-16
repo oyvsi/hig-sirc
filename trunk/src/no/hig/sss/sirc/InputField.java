cae6e1a324b915ba3ecafa06dc6dbac2976cc780
newsflash@gmail.com <newsflash@gmail.com@5554df8d-e30a-845a-8523-bd717c009ae5>
2012-11-16 15:43:29 +0000

    Added: PM and close window. Set focus to input upon tabchange.

--- trunk/src/no/hig/sss/sirc/InputField.java
+++ trunk/src/no/hig/sss/sirc/InputField.java
@@ -2,19 +2,19 @@ package no.hig.sss.sirc;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 
 import javax.swing.JTextField;
 
 public class InputField extends JTextField  {
 	private ConnectionManagement connectionManagement;
-	private TabContainer tabs;
 	private int type;
 	private String identifier;
 		
 	public InputField(int type, String identifier) {
 		super();
 		connectionManagement = sIRC.conManagement;
-		tabs = sIRC.tabContainer;
 		this.type = type;
 		this.identifier = identifier;
 		
@@ -31,36 +31,45 @@ public class InputField extends JTextField  {
 	}
 	//TODO: Needs cleanup
 	public void parseInput(String text) {
-		if(text.charAt(0) == '/') {
-			try {	// Command with argument
-				String[] line = text.split("\\s+");
+		//System.out.println("Input: " + text);
+		if(text.charAt(0) == '/') {	// commands
+			try {
+				String[] line = text.split("\\s+");	// needs try/catch
 				String cmd = line[0].toLowerCase().substring(1);
 				System.out.println("cmd: " + cmd);
-				if(cmd.equals("join")) {
-					System.out.println("join " + line.length);
+					
+				if(cmd.equals("join") || cmd.equals("j")) {
 					if(line.length == 2) { // Where to validate? 
-						System.out.println("All good, passing join");
 						connectionManagement.joinChannel(line[1]);
-					} else {
-						System.out.println("Length is not two");
 					}
 				}
 				else if(cmd.equals("connect")) {
-					connectionManagement.connect("GOLVsirc", line[1]);
+						connectionManagement.connect("FlashSirc", line[1]);
+				}
+				else if(cmd.equals("msg")) {	// Private messages
+					if(line.length > 2) {
+						String msg = "";
+						for(int i = 2; i < line.length; i++)
+							msg = msg.concat(line[i] + " ");
+						msg = msg.substring(0, msg.length()-1);	// Remove last space
+						connectionManagement.privMsg(line[1], msg, TabComponent.PM);
+					}
+				}
+				else if(cmd.equals("wc")) {	 // Close window
+					connectionManagement.closeChat(identifier, type);
 				}
 				else {
-					System.out.println("Unknown command");
+					connectionManagement.channelMsg("Console", "Unknown command", TabComponent.CONSOLE); 
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
-			};
+			}
+			
 		} else {	// Just normal chat
 			if(type == TabComponent.CHANNEL)
 				connectionManagement.channelMsg(identifier, text, type);
 			else
-				connectionManagement.privMsg(identifier, text, type);
-			
+				connectionManagement.privMsg(identifier, text, type);		
 		}	
-	}
-	 
+	}	 
 }

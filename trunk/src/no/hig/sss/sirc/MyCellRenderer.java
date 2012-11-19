a1f44f08f1bd8922432027764c54bc4151633363
bundy.strand@gmail.com <bundy.strand@gmail.com@5554df8d-e30a-845a-8523-bd717c009ae5>
2012-11-19 00:26:01 +0000

    Filtering users depending on mode, and appending prefix on rendering of output

---
+++ trunk/src/no/hig/sss/sirc/MyCellRenderer.java
@@ -0,0 +1,37 @@
+package no.hig.sss.sirc;
+
+import java.awt.Component;
+
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+class MyCellRenderer extends JLabel implements ListCellRenderer<String> {
+	UsersContainer userContainer;
+	
+	public MyCellRenderer(UsersContainer userContainer) {
+		this.userContainer = userContainer;
+	}
+	
+	
+	
+	public Component getListCellRendererComponent(JList list, String value,
+			int index, boolean isSelected, boolean cellHasFocus) {
+		
+		if(!userContainer.getOpList().isEmpty() && userContainer.getOpList().contains(value)) {
+			setText('@' + value.toString());
+			return this;
+		}
+		
+		if(!userContainer.getVoiceList().isEmpty() && userContainer.getVoiceList().contains(value)) {
+			setText('+' + value.toString());
+			return this;
+		}
+		
+		setText(value.toString());
+		
+		return this;
+	}
+
+	
+}

bc48a9e2f775000444ff3132ad0f445d9141f3ad
bundy.strand <bundy.strand@gmail.com>
2012-11-24 01:46:01 +0000

    *Join and part of users *mode change

--- src/no/hig/sss/sirc/UsersContainer.java
+++ src/no/hig/sss/sirc/UsersContainer.java
@@ -6,27 +6,31 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
 
+import javax.swing.AbstractListModel;
+import javax.swing.DefaultListModel;
+
 import jerklib.events.modes.ModeAdjustment.Action;
 
-public class UsersContainer {
-	List<String> users;
+public class UsersContainer extends AbstractListModel {
 	List<String> op;
 	List<String> voice;
-	List<String> regular;
-	Vector<String> usersForView;
+	List<String> regulars;
+	ArrayList<String> usersForView;
 	ConnectionManagement cm = sIRC.conManagement;
+	String channel;
+	boolean opExist = false;
+	boolean voiceExist = false;
+	boolean regularsExist = false;
 	
 	public UsersContainer(String identifier) {
-		op = new ArrayList<String>();
-		voice = new ArrayList<String>();
-		users = new ArrayList<String>(cm.getUsers(identifier));
-		usersForView = new Vector<String>();
-		System.out.println(users);
-		List<String> tmpusers = new ArrayList<String>(users);
+		channel = identifier;
+		List<String> tmpusers =  new ArrayList<String>(cm.getUsers(identifier));
+		usersForView = new ArrayList<String>();
 		System.out.println(tmpusers);
 			
 		
 			if(cm.getUsersMode(identifier, Action.PLUS, 'o').iterator().hasNext())  {
+				opExist = true;
 				Iterator<String> opUser = cm.getUsersMode(identifier, Action.PLUS, 'o').iterator();
 				op = cm.getUsersMode(identifier, Action.PLUS, 'o');
 				while(opUser.hasNext()) {
@@ -40,6 +44,7 @@ public class UsersContainer {
 			}
 				
 			if(cm.getUsersMode(identifier, Action.PLUS, 'v').iterator().hasNext()) {
+				voiceExist = true;
 				Iterator<String> voiceUser = cm.getUsersMode(identifier, Action.PLUS, 'v').iterator();
 				voice = cm.getUsersMode(identifier, Action.PLUS, 'v');
 				while(voiceUser.hasNext()) {
@@ -51,20 +56,57 @@ public class UsersContainer {
 				Collections.sort(voice, String.CASE_INSENSITIVE_ORDER);
 				usersForView.addAll(voice);
 			}
-			System.out.println(tmpusers);
-			regular = new ArrayList<String>(tmpusers);
-			System.out.println(regular);
-			Collections.sort(regular, String.CASE_INSENSITIVE_ORDER);
-			usersForView.addAll(regular);
-		}
+			
+			regulars = new ArrayList<String>(tmpusers);
+			regularsExist = true;
+			Collections.sort(regulars, String.CASE_INSENSITIVE_ORDER);
+			usersForView.addAll(regulars);
+	}
 	
 
 	
-	public void addUser(Character mode, String nick) {
-		users.add(nick);
+	public void addUser(String nick) {
+		usersForView.add(nick);
+		sortList(usersForView);
+		if(cm.getUsersMode(channel, Action.PLUS, 'o').contains(nick)) {
+			if(!opExist)
+				op = new ArrayList<String>();
+			op.add(nick);
+		} else if  (cm.getUsersMode(channel, Action.PLUS, 'v').contains(nick)) {
+			if(!voiceExist) 
+				voice = new ArrayList<String>();
+			voice.add(nick);
+		}
+		fireContentsChanged(this, 0, usersForView.size());
 	}
 	
-	public Vector<String> getList() {
+	public void removeUser(String identifier, String nick) {
+		usersForView.remove(nick);
+		sortList(usersForView);
+		if(opExist && op.contains(nick)) {
+			op.remove(nick);
+		}
+		if(voiceExist && voice.contains(nick)) {
+			voice.remove(nick);
+		}
+		fireContentsChanged(this, 0, usersForView.size());
+	}
+	
+	
+	public Object getElementAt(int index) {
+		String user = usersForView.get(index);
+		if(opExist && op.contains(user)) return '@' + user;
+		if(voiceExist && voice.contains(user)) return '+' + user;
+		if(regularsExist && regulars.contains(user)) return user;
+		return null;
+		}
+	
+	public int getSize() {
+		return usersForView.size();
+	}
+	
+	
+	public ArrayList<String> getList() {
 		return usersForView;
 	}
 	
@@ -75,5 +117,84 @@ public class UsersContainer {
 	
 	public List<String> getVoiceList() {
 		return voice;
+	}
+	
+	public void sortList(List<String> list) {
+		Collections.sort(list, String.CASE_INSENSITIVE_ORDER);
+			
+	}
+
+	public void opMode(String nick, Action action) {
+		if(!opExist) {
+			op = new ArrayList<String>();
+			opExist = true;
+		} 
+		
+		if(action == Action.PLUS) {
+			if(voiceExist && voice.contains(nick)) {
+				voice.remove(nick);
+				op.add(nick);
+				if(voice.size() == 0) voiceExist = false;
+			} else {
+				regulars.remove(nick);
+				if(regulars.size() == 0) regularsExist = false;
+				op.add(nick);
+			}
+		}
+		
+		if(action == Action.MINUS) {
+			op.remove(nick);
+			regulars.add(nick);
+			regularsExist = true;
+		}
+		
+		updateView();
+		
+	}
+
+	public void voiceMode(String nick, Action action) {
+		System.out.println(nick);
+		if(!voiceExist) {
+			voice = new ArrayList<String>();
+			voiceExist = true;
+		} 
+		if(action == Action.PLUS) {
+			if(opExist && op.contains(nick)) {	
+			op.remove(nick);
+			voice.add(nick);
+			if(op.size() == 0) opExist = false;
+				
+			} else {
+				regulars.remove(nick);
+				voice.add(nick);
+				if(regulars.size() == 0) regularsExist = false;
+			} 
+		}
+		
+		if(action == Action.MINUS) {
+			voice.remove(nick);
+			regulars.add(nick);
+			regularsExist = true;
+		}
+		
+		updateView();
+		
+	}
+
+	public void updateView() {
+		usersForView.clear();
+		if(opExist) { 
+		sortList(op);
+		usersForView.addAll(op);
+		
+		}
+		if(voiceExist) {
+			sortList(voice);
+			usersForView.addAll(voice);
+		}
+	    
+		sortList(regulars);
+	    usersForView.addAll(regulars);
+		fireContentsChanged(this, 0, getSize());
 	}
 }

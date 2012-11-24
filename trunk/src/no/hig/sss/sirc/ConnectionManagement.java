690cfc132e3e0653df3536c2652ec899d6655f71
bundy.strand@gmail.com <bundy.strand@gmail.com@5554df8d-e30a-845a-8523-bd717c009ae5>
2012-11-24 02:21:47 +0000

    Prettier leave/part message

--- trunk/src/no/hig/sss/sirc/ConnectionManagement.java
+++ trunk/src/no/hig/sss/sirc/ConnectionManagement.java
@@ -93,8 +93,8 @@ public class ConnectionManagement implements IRCEventListener {
 			String time = timeFormat.format(date);
 			String userName = pe.getUserName();
 			String nickName = pe.getWho();
-			String actionMsg = time + "-!- " + userName + '@' + pe.getHostName() 
-							   + "  " + sIRC.i18n.getStr("channel.userPart") + "  " + pe.getPartMessage();
+			String actionMsg = time + "-!- " + nickName + " [~" + userName + '@' + pe.getHostName() 
+							   + "]  " + sIRC.i18n.getStr("channel.userPart") + "  " + pe.getPartMessage();
 			sIRC.tabContainer.message(actionMsg, channelName, TabComponent.CHANNEL, TabComponent.INFO);
 			sIRC.tabContainer.userLeft(channelName, nickName);
 		}

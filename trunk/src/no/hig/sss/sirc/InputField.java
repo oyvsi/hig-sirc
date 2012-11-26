e81d6b3321ca88388702375be9fcfee67b896816
bundy.strand@gmail.com <bundy.strand@gmail.com@5554df8d-e30a-845a-8523-bd717c009ae5>
2012-11-26 20:33:10 +0000

    Indent fail

--- trunk/src/no/hig/sss/sirc/InputField.java
+++ trunk/src/no/hig/sss/sirc/InputField.java
@@ -78,87 +78,87 @@ public class InputField extends JTextField {
 	public void parseInput(String text) {
 		if(text.length() > 0) {
 			if(text.charAt(0) == '/') {	// commands
-			try {
-				String[] line = text.split("\\s+");	// Split input on space
-				String cmd = line[0].toLowerCase().substring(1);
-					
-				if(cmd.equals("join") || cmd.equals("j")) { // Join a channel
-					if(line.length == 2)
-						connectionManagement.joinChannel(line[1]);
-				}
-				else if(cmd.equals("connect")) { // Connect to a irc-server
-						String nick = sIRC.options.getNick();
-						if(line.length == 2) {
-							if(nick.length() > 2)
-								connectionManagement.connect(nick, line[1]);
-							else
-								sIRC.tabContainer.consoleMsg(sIRC.i18n.getStr("error.noNick"));
+				try {
+					String[] line = text.split("\\s+");	// Split input on space
+					String cmd = line[0].toLowerCase().substring(1);
+						
+					if(cmd.equals("join") || cmd.equals("j")) { // Join a channel
+						if(line.length == 2)
+							connectionManagement.joinChannel(line[1]);
+					}
+					else if(cmd.equals("connect")) { // Connect to a irc-server
+							String nick = sIRC.options.getNick();
+							if(line.length == 2) {
+								if(nick.length() > 2)
+									connectionManagement.connect(nick, line[1]);
+								else
+									sIRC.tabContainer.consoleMsg(sIRC.i18n.getStr("error.noNick"));
+							}
+					}
+					else if(cmd.equals("msg")) {	// Private messages
+						if(line.length > 2) {
+							connectionManagement.privMsg(line[1], restLine(line, 2));
 						}
-				}
-				else if(cmd.equals("msg")) {	// Private messages
-					if(line.length > 2) {
-						connectionManagement.privMsg(line[1], restLine(line, 2));
 					}
-				}
-				else if(cmd.equals("wc")) {	 // Close window
-					String partMsg = null;
-					if(line.length > 1)
-						partMsg = restLine(line, 1);
-					connectionManagement.closeChat(identifier, type, partMsg);
-				}
-
-				else if(cmd.equals("disconnect") || cmd.equals("quit")) {	// Close server connection
-					String quitMsg = null;
-					if(line.length > 1)
-						quitMsg = restLine(line, 1);
-					connectionManagement.disConnect(quitMsg);
-				}
-				else if(cmd.equals("nick")) {	// Nick change
-					if(line.length == 2) {	
-						String newNick = restLine(line, 1);			
-						if(connectionManagement.validateNick(newNick))
-							connectionManagement.changeNick(newNick);
+					else if(cmd.equals("wc")) {	 // Close window
+						String partMsg = null;
+						if(line.length > 1)
+							partMsg = restLine(line, 1);
+						connectionManagement.closeChat(identifier, type, partMsg);
 					}
-				}
-				
-				else if(cmd.equals("me")) {	// Action msg
-					if(line.length > 1) {
-						String actionMsg = restLine(line, 1);
-						connectionManagement.actionMsg(identifier, actionMsg, type);
+	
+					else if(cmd.equals("disconnect") || cmd.equals("quit")) {	// Close server connection
+						String quitMsg = null;
+						if(line.length > 1)
+							quitMsg = restLine(line, 1);
+						connectionManagement.disConnect(quitMsg);
 					}
-				}
-				
-				else if(cmd.equals("topic")) { // Set topic
-					if(type == TabComponent.CHANNEL && line.length > 1) {
-						String topicMsg = restLine(line, 1);
-						connectionManagement.setTopic(identifier, topicMsg);
+					else if(cmd.equals("nick")) {	// Nick change
+						if(line.length == 2) {	
+							String newNick = restLine(line, 1);			
+							if(connectionManagement.validateNick(newNick))
+								connectionManagement.changeNick(newNick);
+						}
 					}
-				}
-				
-				else if(cmd.equals("away")) { // Set or unset away
-					if(line.length > 1) { // We have argument, set away message
-						String awayMsg = restLine(line, 1);
-						connectionManagement.away(awayMsg);
-					} else { // No argument. Unset away.
-						connectionManagement.away(null);
+					
+					else if(cmd.equals("me")) {	// Action msg
+						if(line.length > 1) {
+							String actionMsg = restLine(line, 1);
+							connectionManagement.actionMsg(identifier, actionMsg, type);
+						}
 					}
+					
+					else if(cmd.equals("topic")) { // Set topic
+						if(type == TabComponent.CHANNEL && line.length > 1) {
+							String topicMsg = restLine(line, 1);
+							connectionManagement.setTopic(identifier, topicMsg);
+						}
+					}
+					
+					else if(cmd.equals("away")) { // Set or unset away
+						if(line.length > 1) { // We have argument, set away message
+							String awayMsg = restLine(line, 1);
+							connectionManagement.away(awayMsg);
+						} else { // No argument. Unset away.
+							connectionManagement.away(null);
+						}
+					}
+					
+					else if(cmd.equals("list")) {
+						connectionManagement.ListChannels();
+					}
+						// Input starts with /, but we haven't implemented the command. Give error.
+					else {
+						sIRC.tabContainer.consoleMsg(sIRC.i18n.getStr("error.unknownCommand"));
+					}
+				} catch (Exception e) {	// Split failed.
+					e.printStackTrace();
 				}
-				
-				else if(cmd.equals("list")) {
-					connectionManagement.ListChannels();
-				}
-					// Input starts with /, but we haven't implemented the command. Give error.
-				else {
-					sIRC.tabContainer.consoleMsg(sIRC.i18n.getStr("error.unknownCommand"));
-				}
-			} catch (Exception e) {	// Split failed.
-				e.printStackTrace();
-			}
-		} else {	// Just normal chat
-			if(type == TabComponent.CHANNEL)
-				connectionManagement.channelMsg(identifier, text);
-			else
-				connectionManagement.privMsg(identifier, text);		
+			} else {	// Just normal chat
+				if(type == TabComponent.CHANNEL)
+					connectionManagement.channelMsg(identifier, text);
+				else
+					connectionManagement.privMsg(identifier, text);		
 			}
 		}
 	}	 
